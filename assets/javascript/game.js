9e8a0630cba4ea1a0815acb50488eb4075357d84
Chris R <chris.rocha@>
2017-04-24 23:48:08 -0500

    Can now print out underscores to the screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,72 +1,92 @@
+var acceptedKeys = "abcdefghijklmnopqrstuvwxyz";
+var wordBank = ["HOT FUZZ", "LETHAL WEAPON", "DIE HARD", "BAD BOYS", "TRUE LIES", "TEAM AMERICA", "THE LONG KISS GOODNIGHT", "PREDATOR", "TOTAL RECALL", "ESCAPE FROM LA", "BLOODSPORT", "DEATHWISH", "INDEPENDENCE DAY", "TRUE ROMANCE"];
+var currentWord = wordBank[Math.floor(Math.random() * wordBank.length)];;
+
 var remainingGuesses = 10;
-var lettersGuessed = [];
+var lettersGuessed = "";
 var wrongLetters = [];
 
-
-var acceptedKeys = "abcdefghijklmnopqrstuvwxyz";
-var wordBank = ["HOT FUZZ", "LETHAL WEAPON", "DIE HARD", "BAD BOYS", "TRUE LIES", "TEAM AMERICA", "THE LONG KISS GOODNIGHT", "PREDATOR"];
-var currentWord = "";
-
 document.onkeyup = function(event) {
+
     userKey = event.key;
-    if (accepteptedKeys.indexOf(userKey) > -1) {
-    	game.evaluateInput(userKey);
+    if (acceptedKeys.indexOf(userKey) > -1) {
+        game.evaluateInput(userKey);
     } else {
-    	alert("This game only accepts letters, and you pressed the " key + " key.")
+        alert("This game only accepts letters, and you pressed the " + userKey + " key.")
     }
 }
 
 var game = {
-	setup: function(){
-		var splitToArr = currentWord.split(" ");
-		var output = "";
+    setup: function(){
+        chosenWord = this.chooseWord(wordBank);
+        var characterCount = chosenWord.replace(/\s/g, "").length;
 
-		for (var i = 0; i < splitToArr.length; i++) {
-			output = output + this.wordSetup(splitToArr[i].length) + " ";
-		}
+        var splitToArr = chosenWord.split(" ");
+        var output = [];
+        
 
-		var characterCount = currentWord.replace(/\s/g, "");
-	}
-	wordSetup: function(length){
-		var hyphens = "";
+        for (var i = 0; i < splitToArr.length; i++) {
+            output.push(this.wordSetup(splitToArr[i].length));
+        }
+        this.printWord(output);
 
-		for (var i = 0; i < length; i++) {
-			hyphens = hyphens + "-";
-		}
+        return chosenWord;
+    },
+    chooseWord: function(){
+        var word = wordBank[Math.floor(Math.random() * wordBank.length)];
+        return word;
+    },
+    wordSetup: function(length){
+        var hyphens = "";
+        var wordContent = document.getElementById("word");
 
-		return hyhens;
+        for (var i = 0; i < length; i++) {
+            hyphens = hyphens + "_ ";
+        }
 
-	}
-	evaluateInput: function(key) {
-		if (lettersGuessed.indexOf(key) = -1) {
+        return hyphens;
 
-			if (currentWord.indexOf(key) > -1) {
-				this.correctAnswer(key);
+    },
+    printWord: function(arr){
+        var nodeText = "";
 
-			} else {
-				wrongLetters.push(key);
-				this.wrongAnswer(key);
+        for (var i = 0; i < arr.length; i++) {
+            nodeText = nodeText + arr[i] + " ";
+        }
+        var para = document.createElement("p");
+        var node = document.createTextNode(nodeText);
+        para.appendChild(node);
 
-			}
+        var element = document.getElementById("word-header");
+        element.appendChild(para);
+    },
+    evaluateInput: function(key){
+        if (lettersGuessed.indexOf(key) > -1) {
 
-			lettersGuessed.push(key);
-			remainingGuesses -= 1;
+            if (currentWord.indexOf(key) > -1) {
+                this.correctAnswer(key);
 
-		} else {
-			//logic to alert user that this key was already used
+            } else {
+                wrongLetters.push(key);
+                this.wrongAnswer(key);
 
-		}
-	}
+            }
 
-	correctAnswer: function(key){
-		var letterIndex = currentWord.indexOf(key);
+            lettersGuessed.push(key);
+            remainingGuesses -= 1;
 
-	}
+        } else {
+            //logic to alert user that this key was already used
 
-	wrongAnswer: function(key){
-		//logic display this new letter in the wrong answers array
+        }
+    },
+    correctAnswer: function(key){
+        var letterIndex = currentWord.indexOf(key);
+        // Make sure to subtract from the variable that is keeping track of the remaining characters
+    },
+    wrongAnswer: function(key){
+        //logic display this new letter in the wrong answers array
 
-	}
-
-	gameOver:
+    }
+    //gameOver:
 }
