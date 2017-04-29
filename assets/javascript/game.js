9d469c9df5fbf88d4d49c783401b1da8055cdf74
Chris R <chris.rocha@>
2017-04-29 18:34:54 -0500

    game is in functioning order and also added audio file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 var acceptedKeys = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var wordBank = ["HOT FUZZ", "LETHAL WEAPON", "DIE HARD", "BAD BOYS", "TRUE LIES", "TEAM AMERICA", "THE LONG KISS GOODNIGHT", "PREDATOR", "TOTAL RECALL", "ESCAPE FROM LA", "BLOODSPORT", "DEATHWISH", "INDEPENDENCE DAY", "TRUE ROMANCE"];
+var wordBank = ["HOT FUZZ", "LETHAL WEAPON", "DIE HARD", "BAD BOYS", "TRUE LIES", "TEAM AMERICA", "THE LONG KISS GOODNIGHT", "PREDATOR", "TOTAL RECALL", "ESCAPE FROM LA", "BLOODSPORT", "DEATHWISH", "INDEPENDENCE DAY", "TRUE ROMANCE", "UNDER SEIGE"];
 var currentWord = "";
 
 var remainingGuesses = 10;
@@ -12,6 +12,9 @@ var characterCount = 0;
 document.onkeyup = function(event) {
 
     userKey = event.key.toUpperCase();
+    if (remainingGuesses === 10) {
+        document.getElementById("panel-heading").innerText = "Press any letter to continue playing";
+    }
     if (acceptedKeys.indexOf(userKey) > -1) {
         game.evaluateInput(userKey);
     } else {
@@ -24,22 +27,10 @@ var game = {
         chosenWord = this.chooseWord(wordBank);
         console.log(chosenWord);
         characterCount = chosenWord.replace(/\s/g, "").length;
-
-        // splitToArr = chosenWord.split("");
-        //realStr = chosen;
-
-        // for (var i = 0; i < splitToArr.length; i++) {
-        //     output(this.wordSetup(splitToArr[i].length));
-        // }
-        // realArr = chosenWord.split(" ");
-        // this.printWord(output);
-        // output = this.wordSetup(splitToArr);
         output = this.wordSetup(chosenWord);
-
         this.printWord(output);
-
+        this.updateChances();
         currentWord = chosenWord;
-
         return chosenWord;
     },
     chooseWord: function(){
@@ -52,7 +43,7 @@ var game = {
 
         for (var i = 0; i < arr.length; i++) {
             if (arr[i] === " ") {
-                hyphens = hyphens + ",";
+                hyphens = hyphens + "\n";
             } else {
             hyphens = hyphens + "_";
             }
@@ -62,14 +53,12 @@ var game = {
 
     },
     printWord: function(word){
-        var nodeText = "";
+        var nodeText = word;
 
-        for (var i = 0; i < word.length; i++) {
-            nodeText = nodeText + word[i] + "\t";
-        }
+
         var div = document.getElementById("word-content");
-        // div.style.whitespace = "pre"
-        div.innerText = nodeText;
+
+        div.innerHTML = nodeText;
     },
     evaluateInput: function(key){
         if (lettersGuessed.indexOf(key) < 0) {
@@ -88,20 +77,13 @@ var game = {
         } 
         this.updateChances();
     },
-    // correctAnswer: function(key){
-    //     this.updateIndex(key); 
-        
-    // },
+
     wrongAnswer: function(key){
         wrongLetters.push(key);
         this.updateLetters();
 
     },
     updateIndex: function(key){
-
-        // if (realArr.length > 1) {
-        //     for (var i = 0; i < realArr.length; i++) {
-        //         for (var j = 0; j < realArr[i].length; j++) {
         var count = 0;
         var pos = 0;
 
@@ -113,65 +95,36 @@ var game = {
           characterCount -= 1;
         }
 
-        
-                    //while (chosenWord.indexOf(key,))
-
-                    
-                    // if (realArr[i].charAt(j) === key) {
-                    //     console.log("before: " + output);
-                    //     output[i] = output[i].substring(0, j) + key + output[i].substring(j + 1);
-                    //     console.log("middle: " + output);
-                    //     output[i] = output[i].slice(j, -1);
-                    //     console.log("after: " + output);
-                    // }
-                    //logic to update the word in that array index
-                    //this.updateWord(indexArray, key);  Use replaceat js ****************
-                // }
-
-                // var regexp = new RegExp('[^ ']', 'g);
-
-                // i = ;
-
-            // }
-
-        // } else {
-        //     for (var j = 0; j < realArr.length; j++) {
-        //        if (realArr[0].charAt(j) === key) {
-        //             console.log("before: " + output);
-        //             output[0] = output[0].substring(0, j) + key + output[0].substring(j , realArr[0].length - 1);
-        //             console.log("after: " + output);
-        //         }
-        //     }
-        // }
-
         this.printWord(output);
 
         if (characterCount === 0) {
             wins += 1;
             this.updateWins();
-            this.gameOver();
+            this.celebrateWin();
+            // this.gameOver();
         }
     },
     updateLetters: function(){
         var nodeText = wrongLetters;
         var div = document.getElementById("guessed");
-        // div.style.whitespace = "pre"
         div.innerText = nodeText;
     },
     updateChances: function() {
         var nodeText = remainingGuesses;
         var div = document.getElementById("chances");
-        // div.style.whitespace = "pre"
         div.innerText = nodeText;
     },
     updateWins: function(){
         var nodeText = wins;
         var div = document.getElementById("wins");
-        // div.style.whitespace = "pre"
-        div.innerText = nodeText;
+        var newDiv = document.createElement("div");
+        newDiv.innerText = nodeText;
+        div.appendChild(newDiv);
     },
     celebrateWin: function() {
-
+        var audio = new Audio('assets/yipee.mp3');
+        audio.play();
+        this.gameOver();
     },
     clearVariables: function(){
         currentWord = "";
@@ -186,6 +139,7 @@ var game = {
         
         this.clearVariables();
         this.setup();
+        document.getElementById("panel-heading").innerText = "PRESS ANY KEY TO BEGIN!";
     },
 
 }
